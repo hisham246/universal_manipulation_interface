e5317d72c8aaa14d5aeffc8582736631447838b2
hisham246 <hishamkhalil.eng@gmail.com>
2025-06-25 17:06:38 -0400

    print debug

--- diffusion_policy/policy/diffusion_unet_image_policy.py
+++ diffusion_policy/policy/diffusion_unet_image_policy.py
@@ -46,6 +46,8 @@ class DiffusionUnetImagePolicy(BaseImagePolicy):
             input_dim = action_dim
             global_cond_dim = obs_feature_dim * n_obs_steps
 
+        print("Input:", input_dim.shape)
+        
         model = ConditionalUnet1D(
             input_dim=input_dim,
             local_cond_dim=None,
