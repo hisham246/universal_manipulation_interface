a1b7258d01a89cee6657d70c4d7277264eb3d828
hisham246 <hishamkhalil.eng@gmail.com>
2025-06-26 11:21:43 -0400

    print debug

--- diffusion_policy/dataset/vic_umi_dataset.py
+++ diffusion_policy/dataset/vic_umi_dataset.py
@@ -365,8 +365,8 @@ class VicUmiDataset(BaseDataset):
             action_stiffness = data['action'][..., 13 * robot_id + 6 : 13 * robot_id + 12]
             action_gripper = data['action'][..., 13 * robot_id + 12 : 13 * robot_id + 13]
 
-            # print(f"action_stiffness: {action_stiffness.shape}")
-            # print(f"action_gripper: {action_gripper.shape}")
+            print(f"action_stiffness: {action_stiffness}")
+            print(f"action_gripper: {action_gripper}")
 
             actions.append(np.concatenate([action_pose, action_stiffness, action_gripper], axis=-1))
 
